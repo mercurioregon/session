3099ab5d943d7b48476ef376629520b3ff000b22
mercurioregon <jamesm620@gmail.com>
2024-05-13 15:44:15 -0700

    route templates incl in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -5,3 +5,41 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 module.exports = router;
+
+const path = require("path")
+
+
+// router.get("/    ", (req,res)=> {
+//     fs.readFile(path.join(__dirname,"     " ),"utf8",(err, data) =>{
+//         if (err){console.log ("err")}
+   
+//         res.json(JSON.parse(data))
+//     } )
+// })
+
+// router.post("/   ", (req,res) => {
+//     console.log(req.body)
+//     fs.readFile(path.join(__dirname,"" ),"utf8",(err, data) =>{
+//         if (err){console.log ("err")}
+
+//         fs.writeFile(path.join(__dirname,"  "  ), JSON.stringify(notes), (err)=> {
+//             if (err){console.log ("err")}
+//             res.send(newNote)
+//            } )
+//         } )
+//     })
+
+//     router.delete("/     ", (req,res) => {
+//         const noteId = parseInt(req.params.id)
+//          fs.readFile(path.join(__dirname,"   " ),"utf8",(err, data) =>{
+//              if (err){console.log ("err")}
+             
+
+//             fs.writeFile(path.join(__dirname,"   "  ), JSON.stringify(), (err)=> {
+//              if (err){console.log ("err")}
+//              res.send(  )
+//             } )
+//          } )
+//      })
+
+     module.exports = router
