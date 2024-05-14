273decd866b968a1ae9055194f0b25073eb7d17c
Austin Marlatt <austin.marlatt1@gmail.com>
2024-05-13 20:42:29 -0700

    added profile.js code into the server.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes');
-const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+// const apiRoutes = require('./api');
 
 // TODO - Create and link additional routes
 
 // router.get("/    ", (req,res)=> {
 //     fs.readFile(path.join(__dirname,"     " ),"utf8",(err, data) =>{
 //         if (err){console.log ("err")}
-   
 //         res.json(JSON.parse(data))
 //     } )
 // })
@@ -29,7 +28,6 @@ const apiRoutes = require('./api');
 //         const noteId = parseInt(req.params.id)
 //          fs.readFile(path.join(__dirname,"   " ),"utf8",(err, data) =>{
 //              if (err){console.log ("err")}
-             
 
 //             fs.writeFile(path.join(__dirname,"   "  ), JSON.stringify(), (err)=> {
 //              if (err){console.log ("err")}
@@ -38,11 +36,7 @@ const apiRoutes = require('./api');
 //          } )
 //      })
 
-module.exports = router
-
-
-
-router.use('/',homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
