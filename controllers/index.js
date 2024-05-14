a5a61e02e6efbd46f7a8f68de63ee6c4b9297634
mercurioregon <jamesm620@gmail.com>
2024-05-13 17:43:46 -0700

    route templates added to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,43 @@ const apiRoutes = require('./api');
 
 // TODO - Create and link additional routes
 
+// router.get("/    ", (req,res)=> {
+//     fs.readFile(path.join(__dirname,"     " ),"utf8",(err, data) =>{
+//         if (err){console.log ("err")}
+   
+//         res.json(JSON.parse(data))
+//     } )
+// })
+
+// router.post("/   ", (req,res) => {
+//     console.log(req.body)
+//     fs.readFile(path.join(__dirname,"" ),"utf8",(err, data) =>{
+//         if (err){console.log ("err")}
+
+//         fs.writeFile(path.join(__dirname,"  "  ), JSON.stringify(notes), (err)=> {
+//             if (err){console.log ("err")}
+//             res.send(newNote)
+//            } )
+//         } )
+//     })
+
+//     router.delete("/     ", (req,res) => {
+//         const noteId = parseInt(req.params.id)
+//          fs.readFile(path.join(__dirname,"   " ),"utf8",(err, data) =>{
+//              if (err){console.log ("err")}
+             
+
+//             fs.writeFile(path.join(__dirname,"   "  ), JSON.stringify(), (err)=> {
+//              if (err){console.log ("err")}
+//              res.send(  )
+//             } )
+//          } )
+//      })
+
+module.exports = router
+
+
+
 router.use('/',homeRoutes);
 router.use('/api', apiRoutes);
 
