e9c87c473c2af2ee010afbed6da8da0303963bcf
Kyler Gibbs <kylergibbs@hotmail.com>
2024-05-14 23:51:13 -0700

    updated Query params.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const newProf = require(`../server.js`).newProf;
 
 router.get (`/`,(req, res) =>{
+    newProf = req.query.displayName;
+    console.log ("HomeRoutes",req.session);
     res.render(`homepage`, {
         newProf,
     });
-    console.log (newProf)
 });
 
 module.exports = router;
