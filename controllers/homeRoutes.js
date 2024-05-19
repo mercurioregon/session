7bccf45f0dcf870564d5b1a727ea6cbe737ffc71
mercurioregon <jamesm620@gmail.com>
2024-05-18 19:28:40 -0700

    routes completed in homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 var ACCESSTOKEN = ""
 
-ACCESSTOKEN = access_token
+
 
 router.get (`/`,(req, res) =>{
     newProf = req.query.displayName;
@@ -30,7 +30,7 @@ const list= await fetch(`https://api.spotify.com/v1/users/{user_id}/playlists`,{
 console.log(await list.json())
 });
 
-router.get(`/currect-user-playlist`,async(req, res) => {
+router.get(`/current-user-playlist`,async(req, res) => {
     console.log(ACCESSTOKEN)
     const list= await fetch("https://api.spotify.com/v1/me/playlists", {
       headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
@@ -38,7 +38,7 @@ router.get(`/currect-user-playlist`,async(req, res) => {
       console.log(await list.json())
     });
 
-    router.get(`/related-artists`,async(req, res) => {
+   router.get(`/related-artists`,async(req, res) => {
         console.log(ACCESSTOKEN)
         const list= await fetch("https://api.spotify.com/v1/artists/{id}/related-artists", {
           headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
@@ -46,11 +46,80 @@ router.get(`/currect-user-playlist`,async(req, res) => {
           console.log(await list.json())
         });
 
+        router.get(`/top-tracks`,async(req, res) => {
+            console.log(ACCESSTOKEN)
+            const list= await fetch("https://api.spotify.com/v1/artists/{id}/top-tracks", {
+              headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
+            });
+              console.log(await list.json())
+            });
 
+            router.put(`/follow-playlist`, async(req,res) => {
+                console.log(ACCESSTOKEN)
+                const list= await fetch(`https://api.spotify.com/v1/playlists/{playlist_id}/followers`,{
+                    headers: {"Authorization": "Bearer "+ ACCESSTOKEN}, body: JSON.stringify({
+                             "public": false })
+                });
+                console.log(await list.json())
+                });
 
+             router.delete(`/unfollow-playlist`, async(req,res) => {
+                console.log(ACCESSTOKEN)
+                const list= await fetch(`https://api.spotify.com/v1/playlists/{playlist_id}/followers`,{
+                headers: {"Authorization": "Bearer "+ ACCESSTOKEN}, body: JSON.stringify({
+                           "public": false })
+                 });
+                 console.log(await list.json())
+                  });
 
+           router.get(`/followed-artists`,async(req, res) => {
+            console.log(ACCESSTOKEN)
+            const list= await fetch("https://api.spotify.com/v1/me/following", {
+             headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
+              });
+             console.log(await list.json())
+              });
 
+                            // type or string
 
+                router.put(`/follow-artists-or-users`, async(req,res) => {
+                 console.log(ACCESSTOKEN)
+                 const list= await fetch(`https://api.spotify.com/v1/me/following"`,{
+                  headers: {"Authorization": "Bearer "+ ACCESSTOKEN}, body: JSON.stringify({
+                            "ids": [
+                            "string"
+                                ]})
+                      });
+                      console.log(await list.json())
+                      });
+
+                      router.delete(`/unfollow-playlist`, async(req,res) => {
+                           console.log(ACCESSTOKEN)
+                           const list= await fetch(`https://api.spotify.com/v1/me/following`,{
+                               headers: {"Authorization": "Bearer "+ ACCESSTOKEN}, body: JSON.stringify({
+                                   "ids": [
+                                    "string"
+                                   ] })
+                           });
+                           console.log(await list.json())
+                           });
+     
+                           router.get(`/user-follows-artists-or-users`,async(req, res) => {
+                            console.log(ACCESSTOKEN)
+                            const list= await fetch("https://api.spotify.com/v1/me/following/contains", {
+                            headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
+                               });
+                              console.log(await list.json())
+                               });              
+
+                               router.get(`/user-follows-playlist`,async(req, res) => {
+                                console.log(ACCESSTOKEN)
+                                const list= await fetch("https://api.spotify.com/v1/playlists/{playlist_id}/followers/contains", {
+                                headers: {"Authorization": "Bearer "+ ACCESSTOKEN}
+                                   });
+                                  console.log(await list.json())
+                                   });              
+    
 
 module.exports = router;
 
