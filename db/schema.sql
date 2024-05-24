e78877736ccb2cd4277d0d71ea5b6a94a81bdb38
Austin Marlatt <austin.marlatt1@gmail.com>
2024-05-23 17:40:08 -0700

    added models

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS session_db;
+-- DROP DATABASE IF EXISTS session_db;
 
 CREATE DATABASE session_db;
--- models/User.js
+++ models/User.js
@@ -2,39 +2,48 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-    passCheck(pass) {
-        return bcrypt.compareSync(pass, this.password);
-    }
-}
+class User extends Model {}
+//     passCheck(pass) {
+//         return bcrypt.compareSync(pass, this.password);
+//     }
+// }
 
 User.init(
     {
         id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey:true,
+            primaryKey: true,
             autoIncrement: true,
         },
-        username: {
+        display_name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        password: {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                len: [6],
-            },
-        },            
+            unique: true,
+        },
+        images: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        refresh_token: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        },
     },
     {
-        hooks: {
-            async beforeCreate(newUser) {
-                newUser.password = await bcrypt.hash(newUser, 4);
-                return newUser;
-            },
-        },
+        // hooks: {
+        //     async beforeCreate(newUser) {
+        //         newUser.password = await bcrypt.hash(newUser, 4);
+        //         return newUser;
+        //     },
+        // },
         sequelize,
         timestamps: false,
         freezeTableName: true,
