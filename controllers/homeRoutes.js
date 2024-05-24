9db87f7746fceeae10feff9405a17f83ce14387b
Austin Marlatt <austin.marlatt1@gmail.com>
2024-05-23 18:09:56 -0700

    added Model user info to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,27 +5,31 @@ const { User } = require('../models');
 // ACCESSTOKEN = access_token
 
 
-router.get (`/`,(req, res) =>{
-  newProf = req.query.displayName;
+router.get (`/`, async (req, res) =>{
+  let newProf = req.query.displayName;
+  const userData = await User.findOne({ where: { display_name: newProf} });
+  const currentUser = userData.get({ plain: true });
+
   console.log ("HomeRoutes", req.session);
   res.render(`homepage`, {
       newProf,
+      currentUser,
   });
 });
 
-router.get (`/profile`, async (req, res) =>{
-  try {
-    const userData = await User.findOne({ where: { display_name: req.query.display_name} });
+// router.get (`/profile`, async (req, res) =>{
+//   try {
+//     const userData = await User.findOne({ where: { display_name: req.query.display_name} });
 
-    const currentUser = userData.map((user) => user.get({ plain: true }));
+//     const currentUser = userData.map((user) => user.get({ plain: true }));
 
-    res.render(`profile`, {
-      ...currentUser,
-  });
-  } catch (err) {
-  res.status(404).json(err);
-  }
-});
+//     res.render(`profile`, {
+//       ...currentUser,
+//   });
+//   } catch (err) {
+//   res.status(404).json(err);
+//   }
+// });
 
 
 module.exports = router;
